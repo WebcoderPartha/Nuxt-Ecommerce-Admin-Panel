24432ac7372d363b22fcbb8b9ac82d63fee1a9e9
WebcoderPartha <parthadev76@gmail.com>
2023-07-08 15:23:26 +0600

    Dashboard design

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,25 @@
 <template>
-    <div>
-        <h2>Dashboard</h2>
+    <div class=" grid grid-cols-1 md:grid-cols-4 items-center gap-6 text-center my-8 mx-8">
+        <div class=" bg-gray-800 shadow-md rounded-sm shadow-gray-500 h-32 text-white">
+            <h3 class="text-xl font-semibold pt-4">Total Category</h3>
+            <div class="text-4xl font-semibold pt-4">35</div>
+        </div>
+        <div class=" bg-gray-800 shadow-md rounded-sm shadow-gray-500 h-32 text-white">
+            <h3 class="text-xl font-semibold pt-4">Total Product</h3>
+            <div class="text-4xl font-semibold pt-4">35</div>
+        </div>
+        <div class=" bg-gray-800 shadow-md rounded-sm shadow-gray-500 h-32 text-white">
+            <h3 class="text-xl font-semibold pt-4">Total Customer</h3>
+            <div class="text-4xl font-semibold pt-4">35</div>
+        </div>
+        <div class=" bg-gray-800 shadow-md rounded-sm shadow-gray-500 h-32 text-white">
+            <h3 class="text-xl font-semibold pt-4">Total Total Order</h3>
+            <div class="text-4xl font-semibold pt-4">35</div>
+        </div>
+        <div class=" bg-gray-800 shadow-md rounded-sm shadow-gray-500 h-32 text-white">
+            <h3 class="text-xl font-semibold pt-4">Total Earn</h3>
+            <div class="text-4xl font-semibold pt-4">35</div>
+        </div>
     </div>
 </template>
 
