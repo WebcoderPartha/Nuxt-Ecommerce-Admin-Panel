590c223422c45ca4ef04e621ed8d98c521021c05
WebcoderPartha <parthadev76@gmail.com>
2023-07-10 16:02:33 +0600

    Loggin successfully

--- pages/login.vue
+++ pages/login.vue
@@ -2,10 +2,10 @@
     <div class=" bg-gray-950 h-screen pt-56">
         <div class="w-[400px] mx-auto bg-gray-900 shadow-sm shadow-gray-500 px-14 pb-10">
             <h2 class="text-2xl text-gray-600 text-center py-4" text-center>Login</h2>
-            <form class="flex flex-col gap-4">
-                <input type="text" placeholder="Username" class="bg-gray-800 text-white px-3 py-1 rounded-md focus:outline-none appearance-none">
-                <input type="password" placeholder="Password" class="bg-gray-800 text-white px-3 py-1 rounded-md focus:outline-none">
-                <button class=" bg-gray-950 hover:bg-black py-1 text-zinc-400 rounded-md">Login</button>
+            <form class="flex flex-col gap-4" @submit.prevent="loginHandler">
+                <input v-model="username" type="text" placeholder="Username" class="bg-gray-800 text-white px-3 py-1 rounded-md focus:outline-none appearance-none">
+                <input type="password" v-model="password" placeholder="Password" class="bg-gray-800 text-white px-3 py-1 rounded-md focus:outline-none">
+                <button type="submit" class=" bg-gray-950 hover:bg-black py-1 text-zinc-400 rounded-md">Login</button>
             </form>
         </div>
     </div>
@@ -36,12 +36,20 @@
     
     const {signIn} = useAuth() 
     const loginHandler = async (e) => {
-        if(username.value.length !== 0 || password.value.length !== 0){
-            const {error, url} = await signIn('credentials', {username:username.value, password: password.value, redirect:false })
+        if(username.value.length > 0 && password.value.length > 0){
+            const {error, url, data} = await signIn('credentials', {username:username.value, password: password.value, redirect:false })
             if(error){
                 alert('You have made a terrible mistake while entering your credentials')
             }else{
                 navigateTo('/')
+                username.value = '';
+                password.value = '';
+                e.target.reset()
+                console.log(data)
+                Toast.fire({
+                    icon: "success",
+                    title: "Loggin SuccessfullY!",
+                });
             }
 
         }else{
--- server/api/auth/[...].ts
+++ server/api/auth/[...].ts
@@ -1,6 +1,6 @@
 import CredentialsProvider from 'next-auth/providers/credentials'
 import { NuxtAuthHandler } from '#auth'
-
+import {PrismaClient} from '@prisma/client'
 
 
 export default NuxtAuthHandler({
@@ -12,18 +12,25 @@ export default NuxtAuthHandler({
         // @ts-expect-error You need to use .default here for it to work during SSR. May be fixed via Vite at some point
     CredentialsProvider.default({
       
-        authorize (credentials: any) {
+        async authorize (credentials: any) {
           
-          const user = { id: '1', name: 'J Smith', username: 'jsmith', password: 'hunter2' }
-          if (credentials?.username === user.username && credentials?.password === user.password) {
+            const prisma = new PrismaClient()
+            const loggInUser = await prisma.user.findFirst({
+                where: {
+                    email: credentials?.username,
+                    password:credentials?.password
+                }
+            })
+            return loggInUser;
+        //   if (credentials?.username === user.username && credentials?.password === user.password) {
        
-            return user
-          } else {
+        //     return user
+        //   } else {
      
       
-            return null
+        //     return null
      
-          }
+        //   }
         }
       })
     ]
