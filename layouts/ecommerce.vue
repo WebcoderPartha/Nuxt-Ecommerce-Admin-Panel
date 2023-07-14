f1abe20e9f29b792026042d900f263cf8cc9b0bd
WebcoderPartha <parthadev76@gmail.com>
2023-07-14 15:30:57 +0600

    Header menu design

--- layouts/ecommerce.vue
+++ layouts/ecommerce.vue
@@ -1,24 +1,36 @@
 <template>
     <div>
-        <div class="bg-gray-900 py-4">
-            <header class="flex justify-between w-[990px] mx-auto">
-                <div>
-                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/2560px-Amazon_logo.svg.png" width="150" alt="">
-                </div>
-                <div class="w-[500px]">
-                    <input  type="text" class="bg-gray-800 text-white px-3 py-2 rounded-md w-full focus:outline-none" placeholder="Search">
-                </div>
-                <div class="flex gap-2">
-                    <div class="text-white pt-1 text-xl hover:rounded-md cursor-pointer">
-                        <Icon name="material-symbols:shopping-cart-sharp" />
+        <div class="bg-gray-900 shadow-sm shadow-gray-700 fixed top-0 w-full">
+            <header class=" w-[990px] mx-auto">
+                <div class="flex justify-between pt-6">
+                    <div>
+                        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/2560px-Amazon_logo.svg.png" width="150" alt="">
                     </div>
-                    <div class="text-white px px-3 py-2 hover:rounded-md cursor-pointer">
-                        <Icon name="fa6-regular:user" />
-                        <span class="ml-2">Sign In</span>
+                    <div class="w-[500px]">
+                        <input  type="text" class="bg-gray-800 text-white px-3 py-2 rounded-md w-full focus:outline-none" placeholder="Search">
+                    </div>
+                    <div class="flex gap-2">
+                        <div class="text-white pt-1 text-xl hover:rounded-md cursor-pointer">
+                            <Icon name="material-symbols:shopping-cart-sharp" />
+                        </div>
+                        <div class="text-white px px-2 py-2 hover:rounded-md cursor-pointer">
+                            <Icon name="fa6-regular:user" />
+                            <span class="ml-2">Sign In</span>
+                        </div>
                     </div>
                 </div>
-            </header>
-    </div>
+                
+                <div class="MenuContainer mt-3 pb-3">
+                    <ul class="flex gap-3 text-white font-semibold">
+                        <li class="py-2 px-2"><NuxtLink to="#">Home</NuxtLink></li>
+                        <li class="py-2 px-2"><NuxtLink to="#">Shop</NuxtLink></li>
+                        <li class="py-2 px-2"><NuxtLink to="#">Account</NuxtLink></li>
+                        <li class="py-2 px-2"><NuxtLink to="#">Category</NuxtLink></li>
+                    </ul>
+                </div>
+            </header>    
+        </div>
+        
     </div>
 </template>
 
