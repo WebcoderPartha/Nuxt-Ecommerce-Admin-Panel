a29b8db1f1f0c6ba9002d16487092cfeefd37607
WebcoderPartha <parthadev76@gmail.com>
2023-07-23 01:07:25 +0600

    Category Border set ang backgroud

--- pages/ecom.vue
+++ pages/ecom.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="w-[990px] mx-auto">
         <div class="flex gap-2">
-            <div class="shadow-sm bg-slate-600 shadow-slate-900 px-6 py-3 w-60">
+            <div class="bg-[#f3fee3] px-6 py-3 w-60">
                <h3>Category</h3>
               <ul>
-                <li class="py-3">
+                <li class="py-3 border-b">
                     <NuxtLink to="#">
                         <div class="flex items-center justify-between">
                         <span> Category One</span>
@@ -12,7 +12,7 @@
                     </div>
                     </NuxtLink>
                 </li>
-                <li class="py-3">
+                <li class="py-3 border-b">
                     <NuxtLink to="#">
                         <div class="flex items-center justify-between">
                         <span> Category One</span>
@@ -20,7 +20,7 @@
                     </div>
                     </NuxtLink>
                 </li>
-                <li class="py-3">
+                <li class="py-3 border-b">
                     <NuxtLink to="#">
                         <div class="flex items-center justify-between">
                         <span> Category One</span>
