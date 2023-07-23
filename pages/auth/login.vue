b744fac6d0f68895775e4f622782722777b61ad9
WebcoderPartha <parthadev76@gmail.com>
2023-07-24 00:16:28 +0600

    Login form validation done

--- pages/auth/login.vue
+++ pages/auth/login.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="max-w-[400px] mx-auto my-16 bg-white rounded-lg border border-slate-300">
        <div class="py-10 px-10">
-            <form>
+            <form @submit.prevent="loginHandler">
                 <div class="mb-6">
                  <h2 class="text-2xl text-center">Login your Account</h2>
             </div>
@@ -9,16 +9,18 @@
                 <label class="">
                     <span>Email or Username</span>
                     <div class="">
-                        <input type="text" class=" bg-gray-100 rounded-md focus:outline-none px-2 py-2 w-full" placeholder="Enter email or username">
+                        <input v-model="form.emailOrUsername" type="text" class=" bg-gray-100 rounded-md focus:outline-none px-2 py-2 w-full" placeholder="Enter email or username">
+                        <span class="text-sm text-red-500">{{ requiredForm?.emailOrUsername }}</span>
                     </div>
                 </label>
                 <label class="">
                     <span>Password</span>
                     <div class="">
-                        <input type="password" class=" bg-gray-100 rounded-md focus:outline-none px-2 py-2 w-full" placeholder="Enter password" autocomplete="on">
+                        <input v-model="form.password" type="password" class=" bg-gray-100 rounded-md focus:outline-none px-2 py-2 w-full" placeholder="Enter password" autocomplete="on">
+                        <span class="text-sm text-red-500">{{ requiredForm?.password }}</span>
                     </div>
                 </label>
-                <button class="bg-black text-white py-2 rounded-md">Login</button>
+                <button type="submit" class="bg-black text-white py-2 rounded-md">Login</button>
                 <div class="flex flex-row items-center mx-auto border rounded-sm px-3 cursor-pointer">
                     <img src="/assets/img/google.svg" width="35" alt="">
                     <span>Sign in with Google</span>
@@ -42,6 +44,32 @@ definePageMeta({
     layout:'ecommerce',
 
 })
+
+// Form State
+const form = useState(() => ({
+    emailOrUsername: '',
+    password: ''
+}))
+
+const requiredForm = useState(() => ({
+    emailOrUsername: '',
+    password: ''
+}))
+
+// Login Method
+const loginHandler = (e) => {
+    if(!form.value.emailOrUsername.length > 0 && !form.value.password.length > 0){
+        requiredForm.value.emailOrUsername = 'Email or Username is required!'
+        requiredForm.value.password = 'Password is required!'
+    }else if (!form.value.emailOrUsername.length){
+        requiredForm.value.emailOrUsername = 'Email or Username is required!'
+    }else if (!form.value.password.length > 0){
+        requiredForm.value.password = 'Password is required!'
+    }else{
+        alert('ok')
+    }
+}
+
 </script>
 
 <style lang="scss" scoped>
