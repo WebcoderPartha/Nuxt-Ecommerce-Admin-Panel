636c3b24b74a0332dbf1f006ea089cf1cc058385
WebcoderPartha <parthadev76@gmail.com>
2023-07-23 22:14:46 +0600

    Login Page fixed

--- pages/auth/login.vue
+++ pages/auth/login.vue
@@ -1,7 +1,8 @@
 <template>
-    <div class="max-w-[400px] mx-auto my-24 bg-white rounded-lg border border-slate-300">
+    <div class="max-w-[400px] mx-auto my-16 bg-white rounded-lg border border-slate-300">
        <div class="py-10 px-10">
-            <div class="mb-6">
+            <form>
+                <div class="mb-6">
                  <h2 class="text-2xl text-center">Create your Account</h2>
             </div>
             <div class="flex flex-col gap-4">
@@ -14,7 +15,7 @@
                 <label class="">
                     <span>Password</span>
                     <div class="">
-                        <input type="password" class=" bg-gray-100 rounded-md focus:outline-none px-2 py-2 w-full" placeholder="Enter password">
+                        <input type="password" class=" bg-gray-100 rounded-md focus:outline-none px-2 py-2 w-full" placeholder="Enter password" autocomplete="on">
                     </div>
                 </label>
                 <button class="bg-black text-white py-2 rounded-md">Login</button>
@@ -22,7 +23,13 @@
                     <img src="/assets/img/google.svg" width="35" alt="">
                     <span>Sign in with Google</span>
                 </div>
+                
+                <div class="flex items-center justify-center py-4 gap-3">
+                    <p>Don't have an account? </p>
+                   <NuxtLink to="/auth/signup">Singup</NuxtLink>
+                </div>
             </div>
+            </form>
        </div>
     </div>
 </template>
