c19acf525a1d8854064cafd5b27e579e743b44d9
WebcoderPartha <parthadev76@gmail.com>
2023-07-24 00:06:39 +0600

    Home Category hidden on mobile device

--- pages/index.vue
+++ pages/index.vue
@@ -5,7 +5,7 @@
         </Head>
         <!-- Category & Banner -->
         <div class="md:flex md:gap-2">
-            <div class="bg-[#f3fee3] px-6 py-3 min-w-[300px] mx-auto">
+            <div class="bg-[#f3fee3] px-6 py-3 min-w-[300px] mx-auto hidden md:block">
                <h3 class="text-xl font-semibold">Categories</h3>
               <ul>
                 <li class="py-3 border-b">
