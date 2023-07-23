2cf721595b18ec82e80c652d9f8c94b19a4a37f4
WebcoderPartha <parthadev76@gmail.com>
2023-07-23 17:30:46 +0600

    Footer design completed

--- components/Footer.vue
+++ components/Footer.vue
@@ -19,17 +19,25 @@
                     <p>Phone: <a href="tel:+8801710147887">+880171-0147887</a></p>
                     <p>Email: <a href="mailto:parthadev76@gmail.com">parthadev76@gmail.com</a></p>
                 </div>
+                <div class="text-white">
+                    <h2 class="text-2xl font-semibold mb-3">Let Us Help You</h2>
+                    <ul>
+                        <li><a href="">Your Account</a></li>
+                        <li><a href="">Your Order</a></li>
+                        <li><a href="">Track Your order</a></li>
+                    </ul>
+                </div>
                 <div>
-                    <div class="flex ">
-                        <h2>Social</h2>
-                        <span>
-                            <a href=""><Icon name="mdi:facebook" /></a>
-                        </span>
+                    <h2 class="text-2xl font-semibold mb-3 text-white">Find Us on Google Map</h2>
+                    <div class="px-3 md:px-0">
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3649.5776227640868!2d90.36647477521367!3d23.833614178613736!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755c14a03cac2f3%3A0x9ed3919c3c9f3f5e!2sWeb%20Coder%20Partha!5e0!3m2!1sen!2sbd!4v1690111349394!5m2!1sen!2sbd" width="100%" height="100%" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                     </div>
                 </div>
-                <div>
-                    <h2>ksdjflsd</h2>
-                </div>
+            </div>
+        </div>
+        <div class="footer bg-[#222836] py-6">
+            <div class="max-w-[1200px] mx-auto text-center">
+                <p class=" text-gray-400 text-base">&copy; 2023 Parthadeb Mondal. All rights reserved.</p>
             </div>
         </div>
     </div>
