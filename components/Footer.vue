834effeb07efb9c746f78c6b8275e531fea16531
WebcoderPartha <parthadev76@gmail.com>
2023-07-23 17:18:34 +0600

    Footer designing

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,6 +1,37 @@
 <template>
     <div>
-
+        <div class="footer bg-[#111926] py-16">
+            <div class="max-w-[1200px] mx-auto grid grid-cols-1 md:grid-cols-4 text-center gap-4">
+                <div>
+                    <img class="mx-auto mb-3" src="/assets/img/whitelogo.png" width="170" alt="">
+                    <p class="text-white mb-4">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Necessitatibus optio doloremque, et tenetur, ad accusamus rem mollitia exercitati</p>
+                    <div class="flex gap-3 items-center justify-center">
+                        <h2 class="text-xl text-white mt-2">Follow Us</h2>
+                        <a href="" class="text-white text-2xl"><Icon name="mdi:facebook" /></a>
+                        <a href="" class="text-white text-2xl"><Icon name="mdi:twitter" /></a>
+                        <a href="" class="text-white text-2xl"><Icon name="mdi:youtube" /></a>
+                    </div>
+                </div>
+                <div class="text-white">
+                    <h2 class="text-2xl font-semibold mb-3">Contact Us</h2>
+                    <p >Uttar Kalshi, Block-Pa, Section-12</p>
+                    <p class="mb-3">Pallabi, Mirpur, Dhaka-1216</p>
+                    <p>Phone: <a href="tel:+8801710147887">+880171-0147887</a></p>
+                    <p>Email: <a href="mailto:parthadev76@gmail.com">parthadev76@gmail.com</a></p>
+                </div>
+                <div>
+                    <div class="flex ">
+                        <h2>Social</h2>
+                        <span>
+                            <a href=""><Icon name="mdi:facebook" /></a>
+                        </span>
+                    </div>
+                </div>
+                <div>
+                    <h2>ksdjflsd</h2>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
