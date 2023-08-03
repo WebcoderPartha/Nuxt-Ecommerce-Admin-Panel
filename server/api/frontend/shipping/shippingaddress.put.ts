0a7e6831a4831a091be75f6733d919cc00b0ef71
WebcoderPartha <parthadev76@gmail.com>
2023-08-03 20:50:24 +0600

    Checkout shipping address update apis

---
+++ server/api/frontend/shipping/shippingaddress.put.ts
@@ -0,0 +1,30 @@
+import {Prisma, PrismaClient} from '@prisma/client'
+
+
+export default defineEventHandler( async (event) => {
+
+
+    const getBody = await readBody(event);
+    const prisma = new PrismaClient()
+
+    const category = await prisma.shipping.update({
+        where: {
+            userId: getBody.userId
+        },
+        data: {
+            address_one: getBody.address_one,
+            address_two: getBody.address_two,
+            thana: getBody.thana,
+            zipecode: getBody.zipecode,
+            city: getBody.city,
+            country: getBody.country,
+        }
+    })
+
+    const data = {
+        success: 'Data inserted successfully!'
+    }
+
+    return data
+
+})
