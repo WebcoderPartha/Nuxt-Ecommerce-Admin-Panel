c4d3b86aa6cbc5125a40048cccfc1a675144502f
WebcoderPartha <parthadev76@gmail.com>
2023-08-05 12:38:59 +0600

    Frontend customer orders fetching

---
+++ server/api/frontend/myaccount/orders/[userid].get.ts
@@ -0,0 +1,21 @@
+import { Prisma, PrismaClient } from "@prisma/client"
+
+export default defineEventHandler(async (event) => {
+
+    const prisma = new PrismaClient()
+    const content =  event.context.params
+
+    const orders = await prisma.order.findMany({
+        where: {
+            userId: parseInt(content?.userId)
+        },
+        include: {
+            orderdetails: true
+        }
+    })
+
+    return orders
+
+
+
+})
