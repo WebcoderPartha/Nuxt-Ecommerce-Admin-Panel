d891ce13bce7023a23f507348cc5dbea58f9ed84
WebcoderPartha <parthadev76@gmail.com>
2023-08-05 13:10:17 +0600

    My account all order fetching done

--- pages/my-account.vue
+++ pages/my-account.vue
@@ -18,34 +18,38 @@
             <div v-if="tabState === 'orderlist'" >
                 <div class="flex flex-col gap-3">
                     <h2 class="text-center text-xl bg-gray-100 py-2">Order List</h2>
-                    <table class="w-full border-collapse border border-slate-200">
+                    <table class="w-full border-collapse border border-slate-200" v-if="getOrders?.length > 0">
                         <thead>
                             <tr>
                                 <th class="border border-slate-300">Order ID</th>
                                 <th class="border border-slate-300">Total Price</th>
-                                <th class="border border-slate-300">Total Qty</th>
                                 <th class="border border-slate-300">Transation ID</th>
                                 <th class="border border-slate-300">Payment Method</th>
                                 <th class="border border-slate-300">Order Date</th>
                                 <th class="border border-slate-300">Order Status</th>
-                                <th class="border border-slate-300">Action</th>
+                                <th class="border border-slate-300">Detail</th>
                             </tr>
                         </thead>
                         <tbody>
-                            <tr>
-                                <td class="text-center border border-slate-200">34345</td>
-                                <td class="text-center border border-slate-200">100 BD</td>
-                                <td class="text-center border border-slate-200">100</td>
-                                <td class="text-center border border-slate-200">345345</td>
-                                <td class="text-center border border-slate-200">Cash on delivery</td>
-                                <td class="text-center border border-slate-200">pending</td>
-                                <td class="text-center border border-slate-200">pending</td>
+                            <tr v-for="(order,i) in getOrders" :key="i">
+                                <td class="text-center border border-slate-200">{{order.tcn}}</td>
+                                <td class="text-center border border-slate-200">BDT {{order.total_price}}</td>
+                                <td class="text-center border border-slate-200">{{order.payment_method}}</td>
+                                <td class="text-center border border-slate-200">{{order.tran_id}}</td>
+                                <td class="text-center border border-slate-200">{{order.order_date}}</td>
+                                <td class="text-center border border-slate-200">
+                                    <span class=" bg-yellow-400 text-sm px-2 py-1 rounded-md" v-if="order.order_status === '1'">Pending</span>
+                                    <span class=" bg-blue-400 text-sm px-2 py-1 rounded-md" v-else-if="order.order_status === '2'">Processing</span>
+                                    <span class=" bg-green-400 text-sm px-2 py-1 rounded-md" v-else-if="order.order_status === '3'">Delivered</span>
+                                    <span class=" bg-red-400 text-sm px-2 py-1 rounded-md text-white" v-else>Cancelled</span>
+                                </td>
                                 <td class="text-center border border-slate-200 text-red-400">
                                     <Icon name="entypo:eye" class=" cursor-pointer text-2xl" />
                                 </td>
                             </tr>
                         </tbody>
                     </table>
+                    <h2 class="text-center" v-else>You have no order yet!</h2>
                 </div>
             </div>
             <!-- Order List -->
@@ -74,10 +78,11 @@
 
     const {data:authUser} = useAuth()
 
+    const getOrders = useState(() => [])
     const {data:allOrders} = await useFetch('/api/frontend/myaccount/orders/'+authUser?.value?.user?.id,{
         method: 'GET'
     })
-    console.log(allOrders.value)
+    getOrders.value = allOrders
 
 
 </script>
