2a7b2396e3afdeeb5e0a7e3e15c1b1b7cf95e6d6
WebcoderPartha <parthadev76@gmail.com>
2023-08-05 17:54:51 +0600

    Frontend product details showing done

--- pages/customer/order/view/[orderid].vue
+++ pages/customer/order/view/[orderid].vue
@@ -1,7 +1,7 @@
 <template>
     <div class="max-w-[1200px] mx-auto py-8">
         <div class=" grid grid-cols-1 md:grid-cols-2 gap-6 justify-center">
-            <div class=" shadow-md shadow-gray-400 px-4 py-2">
+            <div class=" shadow-md shadow-gray-400 px-4 py-2 mx-4 md:mx-0">
                 <h2 class="text-center text-xl mb-3">Order Details</h2>
                 <div class="flex justify-between gap-3 py-2 px-4 border-b">
                     <span class="text-base">Customer Name</span>
@@ -20,7 +20,7 @@
                     <span class=" text-base">{{ orderDetail?.order_date }}</span>
                 </div>
             </div>
-            <div class=" shadow-md shadow-gray-400 px-4 py-2">
+            <div class=" shadow-md shadow-gray-400 px-4 py-2 mx-4 md:mx-0">
                 <div class="flex flex-col gap-3">
                     <h2 class="text-center text-xl mb-3">Shipping Address</h2>
                     <div class=" italic">
@@ -28,6 +28,40 @@
                         <p>{{ shippingAddress.address_one }} {{ shippingAddress.address_two }}, {{ shippingAddress.thana }}, {{ shippingAddress.city }}-{{ shippingAddress.zipecode }}, {{ shippingAddress.country }} </p>
                     </div>
                 </div>
+            </div>
+        </div>
+
+        <div class="flex gap-4 py-6">
+            <div class="grid grid-cols-1 md:grid-cols-4 gap-2 w-[800px]">
+                <div class="border border-slate-200 flex flex-col gap-1 pb-2" v-for="(product,i) in orderDetail.orderdetails" :key="i">
+                    <NuxtImg :src="product.image" />
+                    <h2 class="text-center">{{ product.product_name }}</h2>
+                    <p class="text-center text-sm">BDT {{ product.price }}</p>
+                    <p class="text-center text-sm">Qty: {{ product.quantity }}</p>
+                    <p class="text-center text-sm">Subtotal: BDT {{ product.subtotal }}</p>
+                </div>
+        
+            </div>
+            <div class="border border-slate-200 w-[350px] h-[245px] px-4 py-2">
+                <h3 class="text-xl mb-3 mt-3">Total Summary</h3>
+                <div class="flex justify-between">
+                    <span>Subtotal</span>
+                    <span>BDT {{ orderDetail.total_price }}</span>
+                </div>
+                <div class="flex justify-between">
+                    <span>Delivery Fee</span>
+                    <span>BDT 0</span>
+                </div>
+                <div class="flex justify-between">
+                    <span>Vat</span>
+                    <span>BDT 0</span>
+                </div>
+                <hr class="w-full h-px my-4 bg-slate-200 border-0">
+                <div class="flex justify-between mb-2">
+                    <span>Total</span>
+                    <span>BDT {{ orderDetail.total_price }}</span>
+                </div>
+                <p>Paid by {{ orderDetail.payment_method }}</p>
             </div>
         </div>
      
