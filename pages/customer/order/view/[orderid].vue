e718487ed77203b103a12549a7136724b12d0b06
WebcoderPartha <parthadev76@gmail.com>
2023-08-05 17:05:57 +0600

    Shipping address getting

--- pages/customer/order/view/[orderid].vue
+++ pages/customer/order/view/[orderid].vue
@@ -1,6 +1,35 @@
 <template>
     <div class="max-w-[1200px] mx-auto py-8">
-        <h2>{{orderId}}</h2>
+        <div class=" grid grid-cols-1 md:grid-cols-2 gap-6 justify-center">
+            <div class=" shadow-md shadow-gray-400 px-4 py-2">
+                <h2 class="text-center text-xl mb-3">Order Details</h2>
+                <div class="flex justify-between gap-3 py-2 px-4 border-b">
+                    <span class="text-base">Customer Name</span>
+                    <span class=" text-base">{{ authUser?.user?.name }}</span>
+                </div>
+                <div class="flex justify-between gap-3 py-2 px-4 border-b">
+                    <span class="text-base">Order ID</span>
+                    <span class=" text-base">{{ orderDetail?.tcn }}</span>
+                </div>
+                <div class="flex justify-between gap-3 py-2 px-4 border-b">
+                    <span class="text-base">Payment Method</span>
+                    <span class=" text-base">{{ orderDetail?.payment_method }}</span>
+                </div>
+                <div class="flex justify-between gap-3 py-2 px-4 border-b">
+                    <span class="text-base">Order Date</span>
+                    <span class=" text-base">{{ orderDetail?.order_date }}</span>
+                </div>
+            </div>
+            <div class=" shadow-md shadow-gray-400 px-4 py-2">
+                <div class="flex flex-col gap-3">
+                    <h2 class="text-center text-xl mb-3">Shipping Address</h2>
+                    <div class=" italic">
+                        <h4 class=" font-semibold">{{ authUser?.user?.name }}</h4>
+                        <p>{{ shippingAddress.address_one }} {{ shippingAddress.address_two }}, {{ shippingAddress.thana }}, {{ shippingAddress.city }}-{{ shippingAddress.zipecode }}, {{ shippingAddress.country }} </p>
+                    </div>
+                </div>
+            </div>
+        </div>
      
     </div>
   </template>
@@ -27,7 +56,10 @@
     const {data:orderDetail} = await useFetch(`/api/frontend/myaccount/orders/view/${orderId}`, {
         method: 'GET'
     })
-    console.log(orderDetail.value)
+    // console.log(orderDetail.value)
+
+    const {data:shippingAddress} = await useFetch(`/api/frontend/shipping/${authUser?.value?.user?.id}`)
+    
 
     
   
