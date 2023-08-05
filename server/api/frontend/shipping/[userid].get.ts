94fa18cee224e5de466a35cfe64ee83d9db835ba
WebcoderPartha <parthadev76@gmail.com>
2023-08-05 17:10:36 +0600

    API route protected with auth user

--- server/api/frontend/shipping/[userid].get.ts
+++ server/api/frontend/shipping/[userid].get.ts
@@ -1,19 +1,29 @@
 import {Prisma, PrismaClient} from '@prisma/client'
-
+import {getServerSession} from '#auth'
 
 export default defineEventHandler( async (event) => {
 
+    const session = await getServerSession(event)
+    if(session?.user?.role === 'customer'){
 
-    const context = event.context.params 
-    const prisma = new PrismaClient()
-
-    const shippingAddress = await prisma.shipping.findFirst({
-        where: {
-            userId: parseInt(context?.userid)
-        }
-    })
-
+        const context = event.context.params 
+        const prisma = new PrismaClient()
     
-    return shippingAddress
+        const shippingAddress = await prisma.shipping.findFirst({
+            where: {
+                userId: parseInt(context?.userid)
+            }
+        })
+    
+        return shippingAddress
+
+    }else{
+
+        return {
+            status: 'Unauthenticated',
+            statusCode: 401
+        }
+    }
+
 
 })
--- server/api/frontend/shipping/shippingaddress.put.ts
+++ server/api/frontend/shipping/shippingaddress.put.ts
@@ -1,30 +1,42 @@
 import {Prisma, PrismaClient} from '@prisma/client'
-
+import {getServerSession} from '#auth'
 
 export default defineEventHandler( async (event) => {
 
+    const session = await getServerSession(event)
 
-    const getBody = await readBody(event);
-    const prisma = new PrismaClient()
-
-    const category = await prisma.shipping.update({
-        where: {
-            userId: parseInt(getBody.userId)
-        },
-        data: {
-            address_one: getBody.address_one,
-            address_two: getBody.address_two,
-            thana: getBody.thana,
-            zipecode: getBody.zipecode,
-            city: getBody.city,
-            country: getBody.country,
+    if(session?.user?.role === 'customer'){
+        const getBody = await readBody(event);
+        const prisma = new PrismaClient()
+    
+        const category = await prisma.shipping.update({
+            where: {
+                userId: parseInt(getBody.userId)
+            },
+            data: {
+                address_one: getBody.address_one,
+                address_two: getBody.address_two,
+                thana: getBody.thana,
+                zipecode: getBody.zipecode,
+                city: getBody.city,
+                country: getBody.country,
+            }
+        })
+    
+        const data = {
+            success: 'Data updated successfully!'
         }
-    })
+    
+        return data
 
-    const data = {
-        success: 'Data updated successfully!'
+    }else{
+        
+        return {
+                    status: 'Unauthenticated',
+                    statusCode: 401
+                }
+
     }
 
-    return data
 
 })
