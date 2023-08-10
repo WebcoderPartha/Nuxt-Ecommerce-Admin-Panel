f0e4ff77f6968bc03f427b6fb370c70a9486968d
WebcoderPartha <parthadev76@gmail.com>
2023-08-10 19:28:12 +0600

    Pagination functionality

--- pages/paginate.vue
+++ pages/paginate.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class=" w-[1200px] mx-auto">
+    <div class="max-w-[1200px] mx-auto">
         <h1>Pagination</h1>
         {{ currentPage }}
         <ul class="flex justify-center gap-4">
@@ -11,15 +11,15 @@
 
             <!-- Page Number -->
             <li v-else>
-                <span @click="prevPageHandler" :data-prevPage="prevPage" :data-prevSkip="prevSkip" class=" cursor-pointer bg-green-700 text-white px-2 py-1">Prev</span>
+                <span @click="prevPageHandler" class=" cursor-pointer bg-green-700 text-white px-2 py-1">Prev</span>
             </li>
 
-            <li v-for="number in pageNumber">
+            <li v-for="(number, idx) in pageNumber">
                 <span v-if="currentPage === number.page" class="bg-red-700 text-white px-2 py-1 cursor-not-allowed">
                     {{ number.page }}
                 </span>
                 <span v-else :class="` cursor-pointer bg-green-700 text-white px-2 py-1`" @click="paginateHandler"
-                    :data-skip="number.skip" :data-page="number.page">{{ number.page }}</span>
+                    :data-pageNumbe="number.page">{{ number.page }}</span>
             </li>
             <!-- Page Number -->
 
@@ -28,7 +28,7 @@
                 <span class=" cursor-not-allowed bg-gray-700 text-white px-2 py-1">Next</span>
             </li>
             <li v-else>
-                <span @click="nextPageHandler" :data-nextPage="nextPage" :data-nextSkip="nextSkip" class=" cursor-pointer bg-green-700 text-white px-2 py-1">Next</span>
+                <span @click="nextPageHandler" class=" cursor-pointer bg-green-700 text-white px-2 py-1">Next</span>
             </li>
             <!-- Next Button -->
         </ul>
@@ -42,23 +42,20 @@ definePageMeta({
 })
 
 
-
 const { data: res } = await useFetch('/api/frontend/paginate/allproduct', {
     method: 'GET'
 })
 
 const currentPage = ref(1)
 const perPage = ref(20)
-const skiping = ref(0)
 const total = res.value._count.name
+// Total Page
 const TotalPage = Math.ceil(total / perPage.value)
 const pageNumber = []
-
 for (let i = 0; i < TotalPage; i++) {
-    skiping.value += 20
     const paginateData = {
         page: i + 1,
-        skip: skiping.value,
+        skip: i*perPage.value,
     }
     pageNumber.push(paginateData)
 }
@@ -67,80 +64,77 @@ for (let i = 0; i < TotalPage; i++) {
 const lastPage = pageNumber[pageNumber.length -1].page
 const lastPageNumber = ref(0)
 lastPageNumber.value = lastPage
-
 // Last Page Number
 
-const prevSkip = ref(20)
-const prevPage = ref(1)
-const nextSkip = ref(40)
-const nextPage = ref(2)
+// const prevSkip = ref(20)
+// const prevPage = ref(0)
+// const nextPage = ref(2)
 
 
 // Pagination Button Handler
 const paginateHandler = (e) => {
 
-    // Current Page
-    currentPage.value = parseInt(e.target.getAttribute('data-page'))
-    // Current Skip
-    const currentSkip = parseInt(e.target.getAttribute('data-skip'))
-    // Current Take
-    const currentTake = perPage.value
+    // Get Carrent Page
+    currentPage.value = parseInt(e.target.getAttribute('data-pageNumbe'))
+    // Total Page Item
+    const totalPageItem  = parseInt(currentPage.value * perPage.value).toFixed(0)
+    // Skip Item
+    const skip = totalPageItem - perPage.value
+    // Take Item
+    const take = perPage.value
 
-    // Previous Page
-    prevPage.value = currentPage.value - 1
-    // Previous Skip
-    prevSkip.value = currentSkip - perPage.value
-
-    // Previous Button Data
-
-    //Current Button Data
-    const prevData = {
-        prevPage: prevPage.value,
-        skip: prevSkip.value,
+    const data = {
+        skip: skip,
+        take: take
     }
 
-    //Current Button Data
-    const currentData = {
-        currentPage: currentPage.value,
-        skip: prevSkip.value,
-    }
-    console.log(prevData)
+    console.log(data)
+
 }
 
 // Previous Page Button Handler
 const prevPageHandler = (e) => {
-    // Current Page
-    currentPage.value = parseInt(e.target.getAttribute('data-prevPage'))
-    // Current Skip
-    const currentSkip = parseInt(e.target.getAttribute('data-prevSkip')) - perPage.value
-    // Current Take
-    const currentTake = perPage.value
-
-    // Previous Page
-    prevPage.value = currentPage.value - 1
-    // Previous Skip
-    prevSkip.value = currentSkip
-
-    // Previous Button Data
-    const prevData = {
-        prevPage: prevPage.value,
-        skip: prevSkip.value,
-    }
     
-    // Current Button Data
-    const currentData = {
-        currentPage: currentPage.value,
-        skip: currentSkip
+    // Get Previous Page
+    const previousPage = currentPage.value - 1
+    // Get Carrent Page
+    currentPage.value = previousPage
+    // Total Page Item
+    const totalPageItem  = parseInt(previousPage * perPage.value).toFixed(0)
+    // Skip Item
+    const skip = totalPageItem - perPage.value
+    // Take Item
+    const take = perPage.value
+
+    const data = {
+        skip: skip,
+        take: take
     }
-    console.log(prevData)
+    console.log(data)
+
 }
+
 
 // Next Page Button Handdler 
 const nextPageHandler = (e) => {
-    // Current Page
-    currentPage.value = parseInt(e.target.getAttribute('data-nextPage'))
-    // next Page
-    nextPage.value = currentPage.value + 1
+
+   // Get Next Page
+    const nextPage = currentPage.value + 1
+    // Get Current Page
+    currentPage.value = nextPage
+    // Total Page Item
+    const totalPageItem  = parseInt(nextPage * perPage.value).toFixed(0)
+    // Skip Item
+    const skip = totalPageItem - perPage.value
+    // Take Item
+    const take = perPage.value
+
+    const data = {
+        skip: skip,
+        take: take
+    }
+
+    console.log(data)   
 }
 
 </script>
