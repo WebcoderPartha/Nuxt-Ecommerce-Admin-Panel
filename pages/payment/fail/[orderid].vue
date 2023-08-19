a1d7cc36d152ee676748cc723a8cd5cd81e534db
WebcoderPartha <parthadev76@gmail.com>
2023-08-19 14:50:21 +0600

    Order fail route getting

---
+++ pages/payment/fail/[orderid].vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script setup>
+definePageMeta({
+    layout: "ecommerce",
+});
+
+onBeforeMount(() => {
+    if (authData && authData?.value?.user?.role !== "customer") {
+        navigateTo("/auth/login");
+    }
+
+});
+
+const orderid = useRoute().params.orderid
+
+
+</script>
+
+<style lang="scss" scoped></style>
